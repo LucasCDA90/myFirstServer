bdfd90ce93bb79a745d1b3bcd0d3ac923f4fba7f
LucasCDA90 <lucas.brg7@gmail.com>
2024-06-03 15:58:42 +0200

    Ajout tests

--- services/UserService.js
+++ services/UserService.js
@@ -1,125 +1,133 @@
 const UserSchema = require('../schemas/User')
-const _ = require("lodash")
+const _ = require('lodash')
 const async = require('async')
 
-// La fonction permet d'ajouter un utilisateur.
-module.exports.addOneUser = function(user, callback) {
-    // N'ajoute que les champs autorisés
-    var new_user = _.pick(user, UserSchema.authorized)
-    new_user.id = _.uniqueId()
-    var required_isnt_include = _.difference(UserSchema.required.sort(), _.keys(_.pick(new_user, UserSchema.required)).sort())
-    var required_is_empty = _.filter(UserSchema.required, (e) => { return _.isEmpty(new_user[e]) })
-    required_is_empty = _.difference( required_is_empty, required_isnt_include)
+function checkSchemaUser(user, callback) {
+    //var element_check = user
+    var element_check = _.pick(user, UserSchema.authorized)
+    var required_isnt_include = _.difference(UserSchema.required.sort(), _.keys(_.pick(element_check, UserSchema.required)).sort())
+    var required_is_empty = _.filter(UserSchema.required, (e) => { return _.isEmpty(element_check[e]) })
+    required_is_empty = _.difference(required_is_empty, required_isnt_include)
     var text_error = ""
-    if (required_isnt_include.length > 0) {
-        text_error += `Une des propriétés requise (${required_isnt_include.join(', ')}) n'est pas inclue. `
-    }
-    if (required_is_empty.length > 0) {
-        text_error += `Une des propriétés requise (${required_is_empty.join(', ')}) est inclue mais vide.`
-    }
+    if (required_isnt_include.length > 0)
+        text_error += `Une des propriétés requis (${required_isnt_include.join(', ')}) n'est pas inclus. `
+    if (required_is_empty.length > 0)
+        text_error += `Une des propriétés requis (${required_is_empty.join(', ')}) est inclus mais vide.`
     var error = {
         msg: text_error,
         key_required_not_include: required_isnt_include,
-        key_required_empty:required_is_empty
+        key_required_empty: required_is_empty
     }
     if (required_isnt_include.length > 0 || required_is_empty.length > 0) {
         callback(error)
-    } else {
-        UserSchema.elements.push(new_user)
-        callback(null, new_user)
+    }
+    else {
+        callback(null, element_check)
     }
 }
 
-// La fonction permet d'ajouter plusieurs utilisateurs
-module.exports.addManyUsers = function(users, callback) {
+// La fonction permet d'ajouter un utilisateur.
+module.exports.addOneUser = function (user, callback) {
+    checkSchemaUser(user, function (err, value) {
+        if (err)
+            callback(err)
+        else {
+            value.id = _.uniqueId()
+            UserSchema.elements.push(value)
+            callback(null, value)
+        }
+    })
+}
+
+// La fonction permet d'ajouter plusieurs utilisateurs.
+module.exports.addManyUsers = function (users, callback) {
     var i = 0;
-    async.map(users, function(user, next, index) {
-        checkSchemaUser(user, function(err, value) {
+    async.map(users, function (user, next) {
+        checkSchemaUser(user, function (err, value) {
             if (err) {
-                err.index = i;
-                next(null, err);
-            } else {
-                next(null, null);
+                err.index = i
+                next(null, err)
             }
-        });
-        i++;
-    }, function(err, val) {
-        var error = _.filter(val, (e) => { return !_.isEmpty(e); });
+            else {
+                next(null, null)
+            }
+            i++;
+        })
+    }, function (err, val) {
+        var error = _.filter(val, (e) => { return !_.isEmpty(e) })
         if (error.length > 0) {
-            callback(error);
-        } else {
-            async.map(users, checkSchemaUser, function(err, val) {
-                console.log(val);
-                var tab = _.map(val, (e) => { e.id = _.uniqueId(); return e; });
-                UserSchema.elements = [...UserSchema.elements, ...tab];
-                callback(null, val);
-            });
+            callback(error)
+        }
+        else {
+            async.map(users, checkSchemaUser, function (err, val) {
+                var tab = _.map(val, (e) => { e.id = _.uniqueId(); return e })
+                UserSchema.elements = [...UserSchema.elements, ...tab]
+                callback(null, val)
+            })
         }
     });
-};
-/* Une fois tous les utilisateurs vérifiés :
-
-Les erreurs sont filtrées.
-Si des erreurs sont trouvées, le callback est appelé avec ces erreurs.
-Si aucune erreur n'est trouvée, async.map est utilisé à nouveau pour vérifier les utilisateurs.
-Chaque utilisateur reçoit un identifiant unique (_.uniqueId()).
-Les utilisateurs vérifiés sont ajoutés à UserSchema.elements.
-Le callback est appelé avec le tableau des utilisateurs ajoutés. */
-
-// La fonction permet de chercher un utilisateur
-module.exports.findOneUser = function() {
-
 }
 
-// La fonction permet de chercher plusieurs utilisateurs
-module.exports.findManyUsers = function() {
-
-}
-
-// La fonction permet de supprimer un utilisateur
-module.exports.deleteOneUser = function() {
-
-}
-
-// La fonction permet de supprimer plusieurs utilisateurs
-module.exports.deleteManyUsers = function() {
-
-}
-
-// La fonction permet de modifier un utilisateur existant.
-module.exports.updateUser = function(userId, updates, callback) {
-    
-    var user = _.find(UserSchema.elements, { id: userId });
-    if (!user) {
-        return callback({ msg: "Utilisateur non trouvé." });
+// La fonction permet de chercher un utilisateur.
+module.exports.findOneUser = function (id, callback) {
+    var user = _.find(UserSchema.elements, ["id", id])
+    if (user) {
+        callback(null, user)
     }
-
-    // N'ajoute que les champs autorisés
-    var updated_user = _.pick(updates, UserSchema.authorized);
-    
-    // Créer une version de l'utilisateur avec les mises à jour appliquées
-    var new_user = _.assign({}, user, updated_user);
-
-    // Vérifier les champs requis
-    var required_is_empty = _.filter(UserSchema.required, (e) => { return _.isEmpty(new_user[e]) });
-    var text_error = "";
-    if (required_is_empty.length > 0) {
-        text_error += `Une des propriétés requise (${required_is_empty.join(', ')}) est inclue mais vide.`;
-    }
-    var error = {
-        msg: text_error,
-        key_required_empty: required_is_empty
-    };
-    if (required_is_empty.length > 0) {
-        callback(error);
-    } else {
-        // Mettre à jour l'utilisateur dans le schéma
-        _.assign(user, updated_user);
-        callback(null, user);
+    else {
+        callback({ error: true, msg: 'Utilisateur not found.', error_type: 'Not-Found' })
     }
 }
 
-// La fonction permet de modifier plusieurs utilisateurs
-module.exports.updateManyUsers = function() {
+// La fonction permet de chercher plusieurs utilisateurs.
+module.exports.findManyUsers = function (ids, callback) {
+    var users = _.filter(UserSchema.elements, (e) => {
+        return ids.indexOf(e.id) > -1
+    })
+    callback(null, users)
+}
+
+// La fonction permet de supprimer un utilisateur.
+module.exports.deleteOneUser = function (id, callback) {
+    var user_index = _.findIndex(UserSchema.elements, ["id", String(id)])
+    if (user_index > -1) {
+        var user_delete = UserSchema.elements.splice(user_index, 1)[0]
+        callback(null, {msg: "Element supprimé.", user_delete: user_delete})
+    }
+    else {
+        callback({error: 1, msg: "L'utilisateur à effacé n'a pas été trouvé. (Id invalide)"})
+    }
+}
+
+// La fonction permet de supprimer plusieurs utilisateurs.
+module.exports.deleteManyUsers = function (ids, callback) {
+    var count_remove = 0
+    for (var i = 0; i < ids.length;i++) {
+        var user_index = _.findIndex(UserSchema.elements, ["id", String(ids[i])])
+        if (user_index > -1)  {
+            count_remove++
+            UserSchema.elements.splice(user_index, 1)
+        }
+
+    }
+    callback(null, {msg: `${count_remove} élément(s) supprimé(s).`, count_remove: count_remove})
+}
+
+// La fonction permet de modifier un utilisateur.
+module.exports.updateOneUser = function (id, user_edition, callback) {
+    var user_index = _.findIndex(UserSchema.elements, ["id", id])
+    var user_tmp = { ...UserSchema.elements[user_index], ...user_edition }
+    checkSchemaUser(user_tmp, function (err, value) {
+        if (err)
+            callback(err)
+        else {
+            UserSchema.elements[user_index] = { ...UserSchema.elements[user_index], ...value }
+            callback(null, UserSchema.elements[user_index])
+        }
+    })
+}
+
+// La fonction permet de modifier plusieurs utilisateurs.
+module.exports.updateManyUsers = function () {
 
 }
--- tests/services/UserService.js
+++ tests/services/UserService.js
@@ -1,97 +1,330 @@
 const UserService = require('../../services/UserService')
+const chai = require('chai');
+let expect = chai.expect;
 
-// ------------ addOneUser ------------ //
-// Avec un utilsiateur valide
-var user_valid = {
+describe("UserService", () => {
+    describe("addOneUser", () => {
+        it("Utilisateur valide. - S", () => {
+            var user_valid = {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont"
+            }
+            UserService.addOneUser(user_valid, function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('id')
+            })
+        })
+        it("Sans nom d'utilisateur. - E", () => {
+            var user_without_username = {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com"
+            }
+            UserService.addOneUser(user_without_username, function (err, value) {
+                expect(err).to.haveOwnProperty('msg')
+                expect(err).to.haveOwnProperty('key_required_not_include').with.lengthOf(1)
+
+            })
+        })
+
+        it("Avec un champs en trop. - S", () => {
+            var user_with_not_authorized_key = {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont",
+                testing: true,
+                phone: "0645102340"
+            }
+            UserService.addOneUser(user_with_not_authorized_key, function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('id')
+                expect(value).not.haveOwnProperty('testing')
+            })
+        })
+
+        it("Avec un champs requis vide. - E", () => {
+            var user_with_not_authorized_key = {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "",
+                testing: true,
+                phone: "0645102340"
+            }
+            UserService.addOneUser(user_with_not_authorized_key, function (err, value) {
+                expect(err).to.haveOwnProperty('msg')
+                expect(err).to.haveOwnProperty('key_required_empty').with.lengthOf(1)
+
+            })
+        })
+
+    })
+    describe("updateOneUser", () => {
+        it("Modification d'un utilisateur correct. - S", () => {
+            UserService.updateOneUser('1', { lastName: "Maurice" }, function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('id')
+                expect(value).to.haveOwnProperty('lastName')
+                expect(value.lastName).to.equal("Maurice")
+            })
+        })
+        it("Modification d'un utilisateur avec un champs requis, vide. - E", () => {
+            UserService.updateOneUser('1', { lastName: "" }, function (err, value) {
+                expect(err).to.haveOwnProperty('msg')
+                expect(err).to.haveOwnProperty('key_required_empty').with.lengthOf(1, "Le tableau n'a pas retourne le nombre correcte d'element empty.")
+            })
+        })
+        it("Modification d'un utilisateur avec un id invalide. - E", () => {
+            UserService.updateOneUser('100', { lastName: "Edouard" }, function (err, value) {
+                expect(err).to.haveOwnProperty('msg')
+                expect(err).to.haveOwnProperty('key_required_empty').with.lengthOf(0)
+                expect(err).to.haveOwnProperty('key_required_not_include').with.lengthOf(0)
+
+            })
+        })
+    })
+    describe("addManyUsers", () => {
+        it("Ajout de plusieurs utilisateurs non correcte. - E", () => {
+
+            var users_tab_error = [{
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont"
+            }, {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "",
+                testing: true,
+                phone: "0645102340"
+            },
+            {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont",
+                testing: true,
+                phone: "0645102340"
+            }, {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com"
+            }]
+            UserService.addManyUsers(users_tab_error, function (err, value) {
+                expect(err).to.have.lengthOf(2);
+                expect(err[0]).to.haveOwnProperty('msg')
+                expect(err[0]).to.haveOwnProperty('key_required_empty').with.lengthOf(1)
+                expect(err[1]).to.haveOwnProperty('msg')
+                expect(err[1]).to.haveOwnProperty('key_required_empty').with.lengthOf(0)
+                expect(err[1]).to.haveOwnProperty('key_required_not_include').with.lengthOf(1)
+
+            })
+        })
+        it("Ajout de plusieurs utilisateurs tous correct. - S", () => {
+            var users_tab_error = [{
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont"
+            }, {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "La",
+                testing: true,
+                phone: "0645102340"
+            },
+            {
+                firstName: "Edouard",
+                lastName: "Dupont",
+                email: "edouard.dupont@gmail.com",
+                username: "edupont",
+                testing: true,
+                phone: "0645102340"
+            }]
+            UserService.addManyUsers(users_tab_error, function (err, value) {
+                expect(value).to.have.lengthOf(users_tab_error.length);
+                value.forEach((e) => {
+                    expect(e).to.be.a('object');
+                    expect(e).to.haveOwnProperty('id')
+                    expect(e).to.haveOwnProperty('lastName')
+
+                })
+            })
+        })
+    })
+    describe("findOneUser", () => {
+        it("Chercher un utilisateur existant correct. - S", () => {
+
+            UserService.findOneUser("1", function (err, value) {
+                expect(value).to.be.a('object');
+                //console.log(err)
+                expect(value).to.haveOwnProperty('id')
+                expect(value).to.haveOwnProperty('lastName')
+                expect(value['id']).to.equal('1')
+            })
+        })
+        it("Chercher un utilisateur non-existant correct. - E", () => {
+
+            UserService.findOneUser("100", function (err, value) {
+                expect(err).to.haveOwnProperty('msg')
+                expect(err).to.haveOwnProperty('error_type')
+                expect(err["error_type"]).to.equal('Not-Found')
+            })
+        })
+
+    })
+    describe("findManyUsers", () => {
+        it("Chercher plusieurs utilisateurs existants. - S", () => {
+            var tabIds = ["1", "2"]
+            UserService.findManyUsers(tabIds, function (err, value) {
+                expect(value).to.have.lengthOf(tabIds.length);
+                value.forEach((e) => {
+                    expect(e).to.be.a('object');
+                    expect(e).to.haveOwnProperty('id')
+                    expect(e).to.haveOwnProperty('lastName')
+                })
+            })
+        })
+        it("Chercher plusieurs utilisateurs qui n'existent pas. - S", () => {
+            var tabIds = ["100", "200"]
+            UserService.findManyUsers(tabIds, function (err, value) {
+                expect(value).to.have.lengthOf(0);
+            })
+        })
+    })
+    describe("deleteOneUser", () => {
+        it("Supprimer un utilisateur qui existe. - S", () => {
+            UserService.deleteOneUser("1", function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('msg')
+                expect(value).to.haveOwnProperty('user_delete')
+                expect(value['user_delete']).to.haveOwnProperty('id')
+                expect(value['user_delete']['id']).to.be.equal('1')
+            })
+        })
+        it("Supprimer un utilisateur qui n'existe pas. - E", () => {
+            UserService.deleteOneUser("100", function (err, value) {
+                expect(err).to.be.a('object');
+                expect(err).to.haveOwnProperty('msg')
+            })
+        })
+    })
+    describe("deleteManyUsers", () => {
+        it("Supprimer plusieurs utilisateurs tous find. - S", () => {
+            var idTab = ["2"]
+            UserService.deleteManyUsers(idTab, function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('msg')
+                expect(value).to.haveOwnProperty('count_remove')
+                expect(value['count_remove']).to.equal(idTab.length)
+            })
+        })
+        it("Supprimer plusieurs utilisateurs pas tous find. - S", () => {
+            var idTab = ["2", "3"]
+            UserService.deleteManyUsers(idTab, function (err, value) {
+                expect(value).to.be.a('object');
+                expect(value).to.haveOwnProperty('msg')
+                expect(value).to.haveOwnProperty('count_remove')
+                expect(value['count_remove']).to.equal(idTab.length-1)
+            })
+        })
+    })
+})
+/* 
+
+
+
+UserService.updateOneUser('1', {lastName: "Maurice"},function(err, value) {
+    if (err)
+        console.log("Une erreur s'est produite.", err.msg)
+    else {
+        console.log(value)
+    }
+}) 
+
+UserService.updateOneUser('1', {lastName: ""},function(err, value) {
+    if (err)
+        console.log("Une erreur s'est produite.", err.msg)
+    else {
+        console.log(value)
+    }
+}) 
+
+
+var users_tab_error = [{
     firstName: "Edouard",
     lastName: "Dupont",
     email: "edouard.dupont@gmail.com",
     username: "edupont"
-}
-/* UserService.addOneUser(user_valid, function(err, value) {
-    if (err)
-        console.log("Une erreur s'est produite.", err.msg)
-    else {
-        console.log(value)
-    }
-}) */
-
-// Sans nom d'utilisateur
-var user_without_username = {
-    firstName: "Edouard",
-    lastName: "Dupont",
-    email: "edouard.dupont@gmail.com"
-}
-UserService.addOneUser(user_without_username, function(err, value) {
-    if (err)
-        console.log("Une erreur s'est produite.", err.msg)
-    else {
-        console.log(value)
-    }
-})
-// Avec un champs en trop
-var user_with_not_authorized_key = {
-    firstName: "Edouard",
-    lastName: "Dupont",
-    email: "edouard.dupont@gmail.com",
-    username: "edupont",
-    testing: true,
-    phone: "0645102340"
-}
- UserService.addOneUser(user_with_not_authorized_key, function(err, value) {
-    if (err)
-        console.log("Une erreur s'est produite.", err.msg)
-    else {
-        console.log(value)
-    }
-})
-// Avec un champs requis vide
-var user_with_not_authorized_key = {
+},{
     firstName: "Edouard",
     lastName: "Dupont",
     email: "edouard.dupont@gmail.com",
     username: "",
     testing: true,
     phone: "0645102340"
-}
-UserService.addOneUser(user_with_not_authorized_key, function(err, value) {
+},
+{
+    firstName: "Edouard",
+    lastName: "Dupont",
+    email: "edouard.dupont@gmail.com",
+    username: "edupont",
+    testing: true,
+    phone: "0645102340"
+},{
+    firstName: "Edouard",
+    lastName: "Dupont",
+    email: "edouard.dupont@gmail.com"
+}]
+
+console.log("ADD MANY")
+UserService.addManyUsers(users_tab_error,function(err, value) {
     if (err)
         console.log("Une erreur s'est produite.", err.msg)
     else {
         console.log(value)
     }
-})
+}) 
 
-var users_tab_error = [
-    {
-        firstName: "Edouard",
-        lastName: "Dupont",
-        email: "edouard.dupont@gmail.com",
-        username: "edupont"
-    },
-    {
-        firstName: "Edouard",
-        lastName: "Dupont",
-        email: "edouard.dupont@gmail.com",
-        username: "La",
-        testing: true,
-        phone: "0645102340"
-    },
-    {
-        firstName: "Edouard",
-        lastName: "Dupont",
-        email: "edouard.dupont@gmail.com",
-        username: "edupont",
-        testing: true,
-        phone: "0645102340"
-    }
-];
 
-console.log("ADD MANY");
-UserService.addManyUsers(users_tab_error, function(err, value) {
-    if (err) {
-        console.log("Une erreur s'est produite.", err.msg);
-    } else {
-        console.log(value);
+var users_tab_error = [{
+    firstName: "Edouard",
+    lastName: "Dupont",
+    email: "edouard.dupont@gmail.com",
+    username: "edupont"
+},{
+    firstName: "Edouard",
+    lastName: "Dupont",
+    email: "edouard.dupont@gmail.com",
+    username: "La",
+    testing: true,
+    phone: "0645102340"
+},
+{
+    firstName: "Edouard",
+    lastName: "Dupont",
+    email: "edouard.dupont@gmail.com",
+    username: "edupont",
+    testing: true,
+    phone: "0645102340"
+}]
+
+console.log("ADD MANY")
+UserService.addManyUsers(users_tab_error,function(err, value) {
+    if (err)
+        console.log("Une erreur s'est produite.", err.msg)
+    else {
+        console.log(value)
     }
-});
+}) 
+
+console.log("CHERCHER PLUSIEURS UTILISATEURS")
+var tabIds = ["100", "200"]
+UserService.findManyUsers(tabIds, function(err, value) {
+    console.log(value)
+}) */
